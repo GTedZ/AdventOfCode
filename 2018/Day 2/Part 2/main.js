8ca148017d3abde9512daa4e575a7e73d837cbb6
GTedZ <Hellis_over@icloud.com>
2022-12-08 20:29:18 +0200

    added day 2

--- 2018/Day 2/Part 2/main.js
+++ 2018/Day 2/Part 2/main.js
@@ -1,3 +1,15 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('inputs.txt').toString().replace(/\r/gm, "");
+var input = fs.readFileSync('inputs.txt').toString().replace(/\r/gm, "").split('\n');
+
+const strings = {};
+
+const templateString = input[0];
+for (let x = 0; x < templateString.length; x++) {
+
+    for (let word of input) {
+        const char = word.charAt(x);
+        
+    }
+
+}
