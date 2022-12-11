53f477aaead924023466c16a012e2a8ec56d6c50
GTedZ <Hellis_over@icloud.com>
2022-12-11 06:23:52 +0200

    updated shits

--- 2022/Day 10/Part 1/main.js
+++ 2022/Day 10/Part 1/main.js
@@ -1,3 +1,24 @@
 var fs = require('fs');
+var input = fs.readFileSync('inputs.txt').toString().replace(/\r/gm, "").split('\n');
+let cycles = [20, 60, 100, 140, 180, 220], cycle = 0, X = 1, sum = 0, currentCycle = cycles.shift();  // pops the first element from the array cycles
 
-var input = fs.readFileSync('inputs.txt').toString().replace(/\r/gm, "");
+input.forEach(line => {
+    let num = 0;
+    if (line == 'noop') cycle++;
+    else {
+        num = parseInt(line.split(' ')[1]);
+        cycle += 2;
+    }
+    checkCycle();
+    X += num;
+})
+
+console.log(sum);
+
+function checkCycle() {
+    if (cycle >= currentCycle) {
+        console.log({ currentCycle, X, total: currentCycle * X })
+        sum += currentCycle * X;
+        currentCycle = cycles.shift();
+    }
+}
--- 2022/Day 10/Part 2/main.js
+++ 2022/Day 10/Part 2/main.js
@@ -1,3 +1,25 @@
 var fs = require('fs');
+var input = fs.readFileSync('inputs.txt').toString().replace(/\r/gm, "").split('\n');
+let cycle = -1, X = 1, str = '';
+input.forEach(line => {
+    let num = 0;
+    if (line == 'noop') {
+        cycle++;
+        drawPixel();
+    } else {
+        num = parseInt(line.split(' ')[1]);
+        cycle++;
+        drawPixel();
+        cycle++;
+        drawPixel();
+    }
+    X += num;
+})
 
-var input = fs.readFileSync('inputs.txt').toString().replace(/\r/gm, "");
+console.log(str);
+
+function drawPixel() {
+    if (cycle % 40 == 0) str += '\n';
+    if (X - 1 <= cycle % 40 && X + 1 >= cycle % 40) str += '#'; else str += '.';
+
+}
